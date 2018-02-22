12cbee84e2a4752be91c60c9deff361b694e6f39
Pete <prasmusse@student.cvtc.edu>
2018-02-22 13:12:53 -0600

    added explosion when bullet hits asteroid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217210610) do
+ActiveRecord::Schema.define(version: 20180221223624) do
+
+  create_table "scores", force: :cascade do |t|
+    t.integer  "scoreID"
+    t.integer  "userID"
+    t.integer  "score"
+    t.integer  "score_level_reached"
+    t.datetime "time"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
