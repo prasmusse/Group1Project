566d8cc9259659cfca2ee185bf1ffd7447b29484
Matthew Sargeant <matthewsargeant@Matthews-MBP.T-mobile.com>
2018-02-24 08:01:23 -0600

    added faker gem, and added 1 to m relationship between user and score

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221223624) do
+ActiveRecord::Schema.define(version: 20180222113213) do
 
   create_table "scores", force: :cascade do |t|
     t.integer  "scoreID"
