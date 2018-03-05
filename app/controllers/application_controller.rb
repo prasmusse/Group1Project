70ea1f61a05fa9694491b3a92f3d72d02c9df530
David M Kurschner <dkurschner1@student.cvtc.edu>
2018-03-04 19:44:53 -0600

    added adminUser and also set up so if a user isn't logged in they get redirected to login page if they try to type in the url directly also made so it you are logged in and try to go to the login pae you are directed to the game page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :admin_user
+
+def notAuthorized
+  if !current_user
+    redirect_to root_path
+  end
+end
+
+def Authorized
+  if current_user
+    redirect_to game_url
+  end
+end
+
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def admin_user
+   current_user.email == 'dkurschner87@gmail.com'
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+before_action :Authorized
   def show
   end
 end
